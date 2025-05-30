577aea7bc6ebf5f8ca2bbd060d2779f1aaaf935f
deokkyu <han14suk@gmail.com>
2025-05-30 15:41:37 +0900

    0530-01 zustand, context

---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+import { myPage } from "../api/auth";
+
+export default function MyPage(){
+    const [member, setMember] = useState();
+
+    useEffect(()=>{
+        const fetchData = async () =>{
+            try {
+                //const m_idx = localStorage.getItem("tokens");
+                // const response = await myPage(m_idx);
+
+                const response = await myPage();
+                console.log(response);
+
+                if(response.data.success){
+                     console.log("member 세팅할 값:", response.data.data); 
+                    setMember(response.data.data);
+                } 
+            } catch (error) {
+                console.error("가져오기 실패", error)
+            }
+        };
+
+      fetchData();
+    },[]);
+    return(
+        <div>
+            {member?(
+            <>
+                <h2>{member.m_name}님 환영합니다.</h2>
+                <div>
+                    <p>아이디 : {member.m_id}</p>
+                    <p>이름 : {member.m_name}</p>
+                    <p>이메일 : {member.m_email}</p>
+                    <p>전화번호 : {member.m_phone}</p>
+                </div>
+             </>       
+            ) :( <p>로딩 중 ... </p>)}
+           
+        </div>
+    )
+}
